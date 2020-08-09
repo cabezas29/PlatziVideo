687069ad1f7d78c9e1ccf0ca827347b67b5ad99e
devcabezas <luiscabezas25@hotmail.com>
2020-08-09 15:46:38 -0500

    Creacion de componentes

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../assets/components/Header.scss';
+
+const Header = () => (
+  <header className='header'>
+    <img className='header__img' src='../assets/logo-platzi-video-Bw2.png' alt='Platzi Video' />
+    <div className='header__menu'>
+      <div className='header__menu--profile'>
+        <img src='../assets/user-icon.png' alt='' />
+        <p>Perfil</p>
+      </div>
+      <ul>
+        <li>
+          <a href='/'>Cuenta</a>
+        </li>
+        <li>
+          <a href='/'>Cerrar Sesion</a>
+        </li>
+      </ul>
+    </div>
+  </header>
+);
+
+export default Header;
---
+++ src/containers/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Header from '../components/Header';
+import Searchs from '../components/Searchs';
+import Categories from '../components/Categories';
+import Carousel from '../components/Carousel';
+import CarouselItem from '../components/CarouselItem';
+import Footer from '../components/Footer';
+
+import '../assets/styles/App.scss';
+
+const App = () => (
+  <div className='App'>
+    <Header />
+    <Searchs />
+
+    <Categories>
+      <Carousel>
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+      </Carousel>
+    </Categories>
+
+    <Footer />
+
+  </div>
+);
+
+export default App;
